4f50fc99bb8256f6df2deda53f3c220a2f090e63
syldysnya <syldysnya@gmail.com>
2021-04-23 14:49:05 -0400

    Still clock

--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -2,19 +2,20 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-    if (!htmlElement.hasChildNodes()) {
-        const p = document.createElement('p');
-        p.innerText = string;
-        htmlElement.append(p);
-    } else {
-        Array.from(htmlElement.childNodes).forEach( child => {
-            // const newItem = arr[arr.length - 1];
-            const p = document.createElement('p');
-            p.innerText = string;
-            htmlElement.append(p);
-        }); 
-        // const arr = Array.from(htmlElement.childNodes);
+    debugger
+    const p = document.createElement('p');
+    p.innerText = string;
+    
+    // if (!htmlElement.hasChildNodes()) {
+    //     htmlElement.append(p);
+    // } else {
+    //     htmlElement.removeChild(htmlElement.lastChild);
+    //     htmlElement.append(p);
+    // }
+    while (htmlElement.firstChild) {
+        htmlElement.removeChild(htmlElement.lastChild);
     }
+    htmlElement.append(p);
 };
 
 htmlGenerator('Party Time.', partyHeader);
